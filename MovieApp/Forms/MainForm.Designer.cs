d6eb9c5c583b8dc3dc1ebc83a19e53515a1f5db8
R0Y4L45 <106491312+R0Y4L45@users.noreply.github.com>
2022-10-19 19:42:01 +0400

    Last

--- MovieApp/Forms/MainForm.Designer.cs
+++ MovieApp/Forms/MainForm.Designer.cs
@@ -28,36 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.btn_more = new System.Windows.Forms.Button();
             this.txt_search = new System.Windows.Forms.TextBox();
             this.btn_search = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.ErrorImage = global::MovieApp.Properties.Resources._255_2550411_404_error_images_free_png_transparent_png;
+            this.pictureBox1.InitialImage = global::MovieApp.Properties.Resources._255_2550411_404_error_images_free_png_transparent_png;
             this.pictureBox1.Location = new System.Drawing.Point(12, 53);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(298, 314);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            // 
-            // btn_more
-            // 
-            this.btn_more.Location = new System.Drawing.Point(12, 382);
-            this.btn_more.Name = "btn_more";
-            this.btn_more.Size = new System.Drawing.Size(298, 53);
-            this.btn_more.TabIndex = 1;
-            this.btn_more.Text = "Show more";
-            this.btn_more.UseVisualStyleBackColor = true;
+            this.pictureBox1.WaitOnLoad = true;
             // 
             // txt_search
             // 
             this.txt_search.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.txt_search.Location = new System.Drawing.Point(12, 12);
             this.txt_search.Name = "txt_search";
-            this.txt_search.PlaceholderText = "Title";
+            this.txt_search.PlaceholderText = "Enter movie name";
             this.txt_search.Size = new System.Drawing.Size(203, 33);
             this.txt_search.TabIndex = 2;
             // 
@@ -71,18 +80,132 @@
             this.btn_search.UseVisualStyleBackColor = true;
             this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.ErrorImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(563, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(184, 259);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox3.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.ErrorImage")));
+            this.pictureBox3.Location = new System.Drawing.Point(563, 295);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(184, 259);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox4.ErrorImage = global::MovieApp.Properties.Resources._255_2550411_404_error_images_free_png_transparent_png;
+            this.pictureBox4.InitialImage = global::MovieApp.Properties.Resources._255_2550411_404_error_images_free_png_transparent_png;
+            this.pictureBox4.Location = new System.Drawing.Point(362, 12);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(184, 259);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.WaitOnLoad = true;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox5.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.ErrorImage")));
+            this.pictureBox5.Location = new System.Drawing.Point(765, 12);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(184, 259);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(12, 380);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 30);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Release date :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(12, 422);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 30);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Genre :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(12, 467);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 30);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Country :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(12, 507);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 30);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "IMDB :";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(322, 445);
+            this.AutoScroll = true;
+            this.BackgroundImage = global::MovieApp.Properties.Resources.red_black_brush_stroke_banner_background_perfect_canva_1361_3597;
+            this.ClientSize = new System.Drawing.Size(964, 591);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.btn_search);
             this.Controls.Add(this.txt_search);
-            this.Controls.Add(this.btn_more);
             this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MinimumSize = new System.Drawing.Size(504, 462);
             this.Name = "MainForm";
-            this.Text = "MainForm";
+            this.Text = "Movie";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,8 +214,15 @@
         #endregion
 
         private PictureBox pictureBox1;
-        private Button btn_more;
         private TextBox txt_search;
         private Button btn_search;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
--- MovieApp/Forms/MainForm.cs
+++ MovieApp/Forms/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using MovieApp.Properties;
+using System.Text.Json;
 
 namespace MovieApp.Forms;
 
@@ -12,20 +13,54 @@ public partial class MainForm : Form
         InitializeComponent();
     }
 
+    private async void MainForm_Load(object sender, EventArgs e)
+    {
+        using HttpClient client = new();
+
+        
+        string jsonResult1 = await client.GetStringAsync($"{_url}t=Limitless");
+        var movie1 = JsonSerializer.Deserialize<Movie>(jsonResult1);
+        pictureBox2.LoadAsync(movie1?.Poster);
+
+
+        string jsonResult2 = await client.GetStringAsync($"{_url}t=Constantine");
+        var movie2 = JsonSerializer.Deserialize<Movie>(jsonResult2);
+        pictureBox3.LoadAsync(movie2?.Poster);
+
+
+        string jsonResult3 = await client.GetStringAsync($"{_url}t=Black Adam");
+        var movie3 = JsonSerializer.Deserialize<Movie>(jsonResult3);
+        pictureBox4.LoadAsync(movie3?.Poster);
+
+        string jsonResult4 = await client.GetStringAsync($"{_url}t=A-team");
+        var movie4 = JsonSerializer.Deserialize<Movie>(jsonResult4);
+        pictureBox5.LoadAsync(movie4?.Poster);
+    }
+
     private async void btn_search_Click(object sender, EventArgs e)
     {
         string search = $"{_url}t={txt_search.Text}";
         using HttpClient client = new();
 
 
-        var jsonResult = await client.GetStringAsync(search);
+        string jsonResult = await client.GetStringAsync(search);
         var movie = JsonSerializer.Deserialize<Movie>(jsonResult);
-        pictureBox1.LoadAsync(movie?.Poster);
-
-
-        //var jsonResult = client.GetStringAsync(search).Result;
-
-        //var movie = JsonSerializer.Deserialize<Movie>(jsonResult);
-        //pictureBox1.Load(movie?.Poster);
+        if (movie?.Poster is null || movie.Poster == "N/A")
+        {
+            pictureBox1.Image = Resources._255_2550411_404_error_images_free_png_transparent_png;
+            label1.Text = "Release date : Error";
+            label2.Text = "Genre : Error";
+            label3.Text = "Country : Error";
+            label4.Text = "IMDB : Error";
+        }
+        else
+        {
+            pictureBox1.LoadAsync(movie?.Poster);
+            label1.Text = $"Release date : {movie?.Released}";
+            label2.Text = $"Genre : {movie?.Genre}";
+            label3.Text = $"Country : {movie?.Country}";
+            label4.Text = $"IMDB : {movie?.imdbRating}";
+        }
     }
+
 }
